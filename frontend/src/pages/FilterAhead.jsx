5c5a06d79b7666562c8fc09366cb72f5f11691ff
Alex Abainza <abainza.alexandra@gmail.com>
2024-03-29 16:55:44 +0800

    added button tio filter screen

--- frontend/src/pages/FilterAhead.jsx
+++ frontend/src/pages/FilterAhead.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import { Dropdown } from "flowbite-react";
+import { Button, Dropdown } from "flowbite-react";
 import { FaFilter, FaLocationArrow, FaBuilding } from "react-icons/fa";
-import person from "../assets/persontyping.jpg"
+import person from "../assets/persontyping.jpg";
 const FilterAhead = () => {
   return (
-    <div className="mt-16 flex flex-row min-h-screen bg-white">
-      <div className="pt-10 w-1/2 flex flex-col gap-10">
-        <h1 className="text-[#30526A] text-center text-5xl ">Let's find internship opportunities near you!</h1>
-        
-        <div className="px-8 flex flex-col gap-5">
-        <Dropdown
-        placement=""
-          label="Job Category"
-          className="bg-white "
-          dismissOnClick={false}
-          style={{ width: '90%',  backgroundColor: "white", color: "gray", borderColor: "#30526A" }}
+    <div className="mt-16 flex flex-row max-h-screen bg-white justify-center">
+      <div className="pt-10 w-1/2 flex flex-col gap-10 items-center">
+        <h1 className="text-[#30526A] text-center text-5xl ">
+          Let's find internship opportunities near you!
+        </h1>
 
-        >
-          <Dropdown.Item>Advertising and Marketing</Dropdown.Item>
-          <Dropdown.Item>Aerospace</Dropdown.Item>
-          <Dropdown.Item>Agriculture</Dropdown.Item>
-          <Dropdown.Item>Computer and Technology</Dropdown.Item>
-          <Dropdown.Item>Entertainment</Dropdown.Item>
-        </Dropdown>
-        
-        <div className="relative " style={{ width: "100%" }}>
+        <div className="px-16 flex flex-col gap-5 w-full">
+          <Dropdown
+            placement=""
+            label="Job Category"
+            className="bg-white "
+            dismissOnClick={false}
+            style={{
+              width: "100%",
+              backgroundColor: "white",
+              color: "gray",
+              borderColor: "#30526A",
+            }}
+          >
+            <Dropdown.Item>Advertising and Marketing</Dropdown.Item>
+            <Dropdown.Item>Aerospace</Dropdown.Item>
+            <Dropdown.Item>Agriculture</Dropdown.Item>
+            <Dropdown.Item>Computer and Technology</Dropdown.Item>
+            <Dropdown.Item>Entertainment</Dropdown.Item>
+          </Dropdown>
+
+          <div className="relative " style={{ width: "100%" }}>
             <input
               type="text"
               id="first_name"
@@ -32,7 +38,7 @@ const FilterAhead = () => {
               placeholder="Search for a specific keyword"
               required
               style={{
-                width: "90%",
+                width: "100%",
                 backgroundColor: "white",
                 color: "gray",
                 borderColor: "#30526A",
@@ -48,7 +54,7 @@ const FilterAhead = () => {
               placeholder="Search for a specific keyword"
               required
               style={{
-                width: "90%", // Adjusted width to match the other inputs
+                width: "100%",
                 backgroundColor: "white",
                 color: "gray",
                 borderColor: "#30526A",
@@ -65,7 +71,7 @@ const FilterAhead = () => {
               placeholder="Search for a specific keyword"
               required
               style={{
-                width: "90%", // Adjusted width to match the other inputs
+                width: "100%",
                 backgroundColor: "white",
                 color: "gray",
                 borderColor: "#30526A",
@@ -74,21 +80,32 @@ const FilterAhead = () => {
             <FaBuilding className="absolute top-1/2 transform -translate-y-1/2 left-2 h-6 text-[#30526A]" />
           </div>
 
+          <Dropdown
+            label="Paid / Not Paid"
+            className="bg-white"
+            dismissOnClick={false}
+            style={{
+              width: "100%",
+              backgroundColor: "white",
+              color: "gray",
+              borderColor: "#30526A",
+            }}
+          >
+            <Dropdown.Item>Paid</Dropdown.Item>
+            <Dropdown.Item>Not Paid</Dropdown.Item>
+          </Dropdown>
 
-        <Dropdown
-          label="Paid / Not Paid"
-          className="bg-white"
-          dismissOnClick={false}
-          style={{ width: '90%', backgroundColor: "white", color: "gray", borderColor: "#30526A"}}
-        >
-          <Dropdown.Item>Paid</Dropdown.Item>
-          <Dropdown.Item>Not Paid</Dropdown.Item>
-        </Dropdown>
-          </div>
+          <button
+            style={{ width: "100%" }}
+            type="button"
+            class="text-white bg-[#5CDB5C] hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          >
+            Green
+          </button>
+        </div>
       </div>
       <div>
-        <img src={person
-        } />
+        <img src={person} />
       </div>
     </div>
   );
