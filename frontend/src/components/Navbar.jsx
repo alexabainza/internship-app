3a5d1a3e0319c7596eb4e72fdd04f057f52e291b
Alex Abainza <abainza.alexandra@gmail.com>
2024-03-29 13:03:21 +0800

    made landing page responsive-ish

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import logo from "../assets/Carbs.svg";
 
 const Navbar = () => {
   return (
-    <nav class="bg-gradient-to-br from-[#074666] to-[#0B0027]  fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 px-8">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav class="bg-gradient-to-br from-[#074666] to-[#0B0027]  fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 px-8 lg:px-32 md:px-16 sm:px-4">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4">
         <Link
           to="/landing"
           class="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img src={logo} class="h-8" alt="Flowbite Logo" />
-          <span class="self-center text-lg font-light whitespace-nowrap text-white">
+          <img src={logo} class="h-8 mx-0 px-0" alt="Flowbite Logo" />
+          <span class="md:inline sm:hidden self-center text-lg font-light whitespace-nowrap text-white">
             Internship Service Platform
           </span>
         </Link>
@@ -19,7 +19,7 @@ const Navbar = () => {
           <Link
             to="/login"
             type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="lg:inline md:inline sm:hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Login
           </Link>
--- frontend/src/pages/credentials/Landing.jsx
+++ frontend/src/pages/credentials/Landing.jsx
@@ -4,43 +4,45 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div className="mt-16 flex flex-row w-full bg-gradient-to-br from-[#074666] to-[#0B0027] min-w-full h-dvh ">
-      <div className="w-3/4 m-32 flex flex-col text-white gap-6">
-        <h1 className="w-5/6 text-7xl font-semibold">
-          An internship page just for you.
-        </h1>
-        <p className="w-5/6 text-xl">
-          All you see are job hunting pages? How about an internship hunting
-          page? Find internship opportunities available and suited for you, all
-          in this internship hunting page.
-          <br/>
-          <br/>
+    <div className="mt-16 md:w-full flex xl:flex-row lg:flex-row md:flex-col sm:flex-col min-h-screen">
 
-          Click on one of the buttons below to get started.
+        <div className="lg:w-1/2 md:w-full sm:w-full md:px-16 lg:m-32 md:m-16 sm:m-4 flex flex-col text-white gap-6">
+          <h1 className="text-3xl md:text-5xl lg:text-7xl font-semibold">
+            An internship page just for you.
+          </h1>
+          <p className="text-lg md:text-xl lg:text-2xl">
+            All you see are job hunting pages? How about an internship hunting
+            page? Find internship opportunities available and suited for you,
+            all in this internship hunting page.
+            <br />
+            <br />
+            Click on one of the buttons below to get started.
+          </p>
 
-        </p>
-
-        <div className="flex"> {/* Added flex and justify-between */}
-        <Link to="/login"
-          type="button"
-          class="text-white text-center w-1/4 bg-[#73d473] hover:bg-[#3adb3a]  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-        >
-          I am a student
-        </Link>
-        <button
-          type="button"
-          class="text-white w-1/4 bg-[#73d473] hover:bg-[#3adb3a]  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-        >
-          I am an employer
-        </button>
+          <div className="flex flex-wrap">
+            {" "}
+            {/* Added flex-wrap */}
+            <Link
+              to="/login"
+              type="button"
+              class="text-white text-center lg:w-1/4 md:w-3/4 sm:w-3/4  bg-[#73d473] hover:bg-[#3adb3a]  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 min-w-[200px]"
+            >
+              I am a student
+            </Link>
+            <button
+              type="button"
+              class="text-white text-center lg:w-1/4 md:w-3/4 sm:w-3/4  bg-[#73d473] hover:bg-[#3adb3a]  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 min-w-[200px]"
+            >
+              I am an employer
+            </button>
+          </div>
         </div>
-        
+        <div className="lg:w-1/3 md:w-1/2  pt-10 self-center">
+          {" "}
+          {/* Adjusted image container width */}
+          <img src={landing}></img>
+        </div>
       </div>
-      <div className="w-1/4 me-32 mt-32 h-full">
-        <img src={landing}></img>
-        Teehee
-      </div>
-    </div>
   );
 };
 
